88af446c55fe6ae5ddb6c6dfa8fcd37b12b06762
William Rosney <willia.rosney11@gmail.com>
2022-10-20 16:56:59 -0400

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/'
+  private currentUserSource = new ReplaySubject<User>(1); //Subject permettant de stocker les infos de notre user dans un buffer
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response; //Récupère les infos du user que nous renvoie la db lors du login post
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user)); //Stock les infos du user localement dans le browser
+          this.currentUserSource.next(user); //Set la next value de notre buffer
+          return user;
+        }
+      })
+    );
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+          return user;
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user'); //Supprime les infos du user du stockage local du browser
+    this.currentUserSource.next(null);
+  }
+
+}
