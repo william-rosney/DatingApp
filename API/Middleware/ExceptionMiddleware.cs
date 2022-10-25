81f86f1fbad6bb1e6e28dd7122f04308fdf83f3d
William Rosney <willia.rosney11@gmail.com>
2022-10-25 13:30:04 -0400

    End of section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Errors;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message); //Permet de logger les erreurs dans le terminal
+                context.Response.ContentType = "application/json"; //Set le content type de la réponse que l'on renvoie suite à la requete HTTP
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError; //Set le StatusCode
+
+                //Test si l'environnement est en mode developpement ou pas
+                var response = _env.IsDevelopment() 
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) // Si oui on détail bien le contenu de notre response
+                : new ApiException(context.Response.StatusCode, "Internal Servor Error"); // Si non on précise juste Internal Servor Error
+
+                //On va convertir notre response en JSON
+                //On commence par set les options pour respecter le camelCase 
+                var options = new JsonSerializerOptions{
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase 
+                };
+
+                //Serialize notre response en JSON
+                var json = JsonSerializer.Serialize(response, options);
+
+                //On envoie le response
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+    }
+}
---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+  get400error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: error => { console.log(error), this.validationErrors = error }
+    });
+  }
+  get401error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+  get500error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+}
